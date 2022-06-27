a64815d6a8e0a6c8053b5be5f163247241f4e5e2
Tasa <greatwillow@mail.ru>
2022-06-27 22:36:31 +0300

    first hw

---
+++ T1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+string numberStr1 = Console.ReadLine();
+int number1 = int.Parse(numberStr1);
+Console.WriteLine("Введите второе число");
+string numberStr2 = Console.ReadLine();
+int number2 = int.Parse(numberStr2);
+if (number1==number2)
+    Console.WriteLine("Числа равны");
+else if (number1>number2)
+    Console.WriteLine("Максимальное число {0}",number1);
+else
+    Console.WriteLine("Максимальное число {0}",number2);
